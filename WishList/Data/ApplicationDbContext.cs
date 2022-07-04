776890d05e14926a2dcd05aee95581b7625aa11b
Lawrence (Drew) Whisenant <7390131+drewwhis@users.noreply.github.com>
2022-07-04 17:54:17 -0400

    create item model with ef support

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
+
